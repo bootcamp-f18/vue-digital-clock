2e966d7a6d025b41993a41f569851746fa066f48
Matthew Gidcomb <matthew.gidcomb@outlook.com>
2018-10-19 08:33:39 -0400

    Revert "changes"
    This reverts commit 8417e213c475efc91a744e01c03ded38c797ec12, reversing
    changes made to 8b164e4de017b9d4d8ae72bc174acd719a5b210a.

--- app.js
+++ app.js
@@ -4,12 +4,7 @@ let app = new Vue({
 
     data: {
 
-        clockstuff: '00:00:00',
-        alarm: '00:00:00',
-        alarmHours: "00",
-        alarmMinutes: "00",
-        alarmSeconds: "00"
-
+        clockstuff: '00:00:00'
 
     },
 
@@ -22,14 +17,12 @@ let app = new Vue({
             self.clockstuff = self.setTime();
         }, 1000);
 
-
-
     },
 
     methods: {
 
         setTime: function () {
-            console.log(this.alarm);
+
             let now = new Date();
 
             let hours = now.getHours();
@@ -46,43 +39,15 @@ let app = new Vue({
             if (seconds < 10) {
                 seconds = '0' + seconds;
             }
-            
-            if (this.clockstuff == this.alarm){
-
-
-              console.log("ALARM");
-
-
-            }
 
             return hours + ':' + minutes + ':' + seconds;
 
-        },
-
-        alarmSet: function () {
-
-          let my = this;
-
-
-          console.log("hello");
-
-
-          console.log(this.alarmHours);
-
-          console.log(this.alarmMinutes);
-
-          console.log(this.alarmSeconds);
-
-          console.log("Alarm is set");
-          this.alarm = this.alarmHours + ':' + this.alarmMinutes + ':' + this.alarmSeconds;
-
-        },
-
-        alarmClear: function () {
-
-          this.alarm = "00:00:00";
         }
 
-      }
+
+    }
+
+
+
 
 });
